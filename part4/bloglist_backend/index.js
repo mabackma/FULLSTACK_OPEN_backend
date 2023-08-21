c9f50209c9bb9bbcbc25a65f3a2a052a605b6389
Marc Bäckman <marc.backman@edu.lapinamk.fi>
2023-08-22 00:37:11 +0300

    bloglist in modules

--- part4/bloglist_backend/index.js
+++ part4/bloglist_backend/index.js
@@ -1,34 +1,7 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app') // the actual Express application
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-app.use(cors())
-app.use(express.json())
-
-// Import the database module
-const Blog = require('./models/blog')
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
--- part4/bloglist_backend/models/blog.js
+++ part4/bloglist_backend/models/blog.js
@@ -1,14 +1,4 @@
 const mongoose = require('mongoose')
-const url = process.env.MONGODB_URI
-
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const blogSchema = new mongoose.Schema({
   title: String,
